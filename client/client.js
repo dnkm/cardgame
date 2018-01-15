f0891c5620d4c577bfe736be9c7057cf429c2ade
daniel kim <daniel.c.kim@live.com>
2018-01-14 18:49:02 -0800

    sdfsdf

--- client/client.js
+++ client/client.js
@@ -1,5 +1,15 @@
 window.WebSocket = window.WebSocket || window.MozWebSocket;
 
+let clientId;
+let player = {
+    hand: [],
+    field: []
+}
+let opp = {
+    hand: [],
+    field: []
+}
+
 let conn = new WebSocket("ws://127.0.0.1:1337");
 conn.onopen = () => {
     console.log("connection is open");
@@ -11,6 +21,8 @@ conn.onmessage = msg => {
     console.log(msg.data);
     let data = JSON.parse(msg.data);
     switch(data.type) {
+        case 'new_user' :
+            clientId = data.clientId;
         case 'draw_card' : 
             let cardDiv = GameGUI.createCard(data.card);
             document.body.appendChild(cardDiv);
